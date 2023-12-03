d46966dfa8f19f14822e74dc52cb93e72a601a8b
m0kr4n3 <jm_abdelmalek@esi.dz>
2023-12-03 22:44:58 +0100

    Update

--- app/src/api/endpoints/login.py
+++ app/src/api/endpoints/login.py
@@ -39,12 +39,9 @@ def login_access_token(
         "token_type": "bearer",
     }
 
-
 @router.post("/login/test-token", response_model=schemas.User)
 def test_token(current_user: models.User = Depends(deps.get_current_user)) -> Any:
     """
     Test access token
     """
     return current_user
-
-
